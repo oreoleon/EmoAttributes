e0465f83a3bbd4c22b947a9c48b9e995c9989c1d
oreoleon <once.reload@gmail.com>
2025-04-22 23:39:18 +0900

    新規立ち上げ #oreoleo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EmoAttributes.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
